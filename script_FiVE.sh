e59260ba1b182b097d459445440f356ebd7bd9f3
MinghanLI <liminghan0330@gmail.com>
2025-03-04 15:17:51 -0500

    update readme

--- script_FiVE.sh
+++ script_FiVE.sh
@@ -4,3 +4,24 @@ CUDA_VISIBLE_DEVICES=2 python run_vidtome.py \
     --config_base configs_FiVE/default.yaml \
     --configs_file data_FiVE/edit_prompt/edit1_FiVE.json \
     --centercrop False
+
+CUDA_VISIBLE_DEVICES=2 python run_vidtome.py \
+    --data_dir data_FiVE/ \
+    --output_dir outputs_FiVE/ \
+    --config_base configs_FiVE/default.yaml \
+    --configs_file data_FiVE/edit_prompt/edit2_FiVE.json \
+    --centercrop False
+
+CUDA_VISIBLE_DEVICES=2 python run_vidtome.py \
+    --data_dir data_FiVE/ \
+    --output_dir outputs_FiVE/ \
+    --config_base configs_FiVE/default.yaml \
+    --configs_file data_FiVE/edit_prompt/edit3_FiVE.json \
+    --centercrop False
+
+CUDA_VISIBLE_DEVICES=2 python run_vidtome.py \
+    --data_dir data_FiVE/ \
+    --output_dir outputs_FiVE/ \
+    --config_base configs_FiVE/default.yaml \
+    --configs_file data_FiVE/edit_prompt/edit4_FiVE.json \
+    --centercrop False
